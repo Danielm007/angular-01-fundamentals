f2bd9150a767d256ff3f71aee52259b88abc8893
Daniel <daniel.morocho@simetrik.com>
2022-10-25 15:50:28 -0500

    Introduccion a angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  // template: ` <h1>Hola de nuevo</h1> `,
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Contador App';
+  number: number = 0;
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.number += valor;
+  }
+
+  // sumar() {
+  //   this.number += 1;
+  // }
+
+  // restar() {
+  //   this.number -= 1;
+  // }
 }
